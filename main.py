b7bd9eb50f9d876b24decef0edb028892e653f7b
PUMBA32 <sokolovgames42@gmail.com>
2025-10-30 00:42:39 +0300

    some updates

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import sys
 import matplotlib.pyplot as plt
 
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Union
 
+
+'''
+Информатика - 27 заданий
+Профильная математика - 12 заданий
+Русский язык - 26 заданий
+'''
 
 def cls() -> None:
     os.system("cls" if sys.platform == 'win32' else 'clear')
@@ -22,6 +28,12 @@ class Constants:
     IT_DATA: str = os.path.join(IT_FOLDER, 'информатика.txt')
     MATH_DATA: str = os.path.join(MATH_FOLDER, 'математика.txt')
     RUS_DATA: str = os.path.join(RUS_FOLDER, 'русский язык.txt')
+
+
+class Diagram:
+    @staticmethod
+    def diagram(data: list[str], optional_params: Optional[str] = None) -> None: 
+        print(optional_params)
 
 
 class FileWorker:
@@ -88,7 +100,7 @@ class FileWorker:
         cls()
         if len(data) > 0:
             for i, row in enumerate(data):
-                print(f"{i+1}. {row}")
+                print(f"{i+1}. {row}", end="")
             print()
         else:
             print("тут ничего нет...\n")
@@ -133,7 +145,7 @@ class Program:
             match choice:
                 case "1": self.add()
                 case "2": self.delete()
-                case "3": self.stats()
+                case "3": self.stats()    
                 case _: return
 
 
@@ -158,13 +170,13 @@ class Program:
 
         # данные сохраняются в формате: дата, количество решенных заданий
         try:
-            count: int = int(input("Количество решенных заданий: ").strip())  # количество решенных заданий
+            count: int = int(input("Количество верно решенных заданий: ").strip())  # количество решенных заданий
         except ValueError as e:
             cls()
-            print(f"Ошибка, вы должны ввести число! {e}")
+            print(f"Ошибка, вы должны ввести число!\n")
             return
         else:
-            row: str = f'{datetime.now().date().strftime("%d/%m/%Y")} {count}'
+            row: str = f'{datetime.now().date().strftime("%d/%m/%Y")} {count}\n'
             FileWorker.write(path, row)
 
     
@@ -178,14 +190,34 @@ class Program:
 
         if len(file_content) > 0:  # если в файле есть какие то данные
             try:
-                cls()
                 index: int = int(input("Номер записи для удаления: ").strip())
-            except ValueError as e:
+            except ValueError:
                 cls()
-                print(f"Ошибка, вы должны ввести число! {e}")
+                print(f"Ошибка, вы должны ввести число!\n")
                 return
             else:
                 FileWorker.delete_row(path, index)
+
+    
+    def stats(self) -> None: 
+        # получаем путь до файла с данными по вводу пользователя и проверяем является ли полученный путь None
+        if (path := self.__path()) == None: return 
+        
+        # получаем содержимое файла и выводим его на экран
+        file_content: list[str] = FileWorker.get_lines(path)
+        FileWorker.show_file_content(file_content)
+
+        if len(file_content) > 0:
+            scores: list[int] = [int(line.split(" ")[1].replace("\n", '')) for line in file_content]
+            
+            count: int = len(scores)
+            max_score: int = max(scores)
+            min_score: int = min(scores)
+            mid: float = round(sum(scores)/len(scores), 2)
+            mid_by_last_values: Union[float, str] = round((scores[-1]+scores[-2]+scores[-3])/count, 2) if count >= 3 else "too less values"
+
+            optional_params: str = f'count of values: {count}\nmax score: {max_score}\nmin score: {min_score}\nmiddle grade: {mid}\nmiddle grade by last 3 values: {mid_by_last_values}'
+            Diagram.diagram(file_content, optional_params)
 
     
 def main() -> None: 
@@ -195,3 +227,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
